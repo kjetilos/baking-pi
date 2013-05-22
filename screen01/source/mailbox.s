282fb9e4b7f8b164891f38aaa29391067c8325ba
Kjetil Østerås <kos@appeartv.com>
2013-05-22 15:21:23 +0200

    Showing something on the screen

--- screen01/source/mailbox.s
+++ screen01/source/mailbox.s
@@ -10,14 +10,47 @@ MailboxWrite:
 	cmp r1, #15
 	movhi pc, lr
 
+	/* r2 = value */
 	mov r2, r0
 	push {lr}
 	bl GetMailboxBase
 wait1$:
+	/* r3 = status */
 	ldr r3, [r0, #0x18]
-	tst status, #0x80000000
+	tst r3, #0x80000000
 	bne wait1$
 
+	/* r1 = channel */
 	add r2, r1
 	str r2, [r0, #0x20]
 	pop {pc}
+
+.globl MailboxRead
+MailboxRead:
+	cmp r0, #15
+	movhi pc, lr
+	mov r1, r0
+	push {lr}
+
+	bl GetMailboxBase
+	/* r0 = mailbox */
+	/* r1 = channel */
+rightmail$:
+wait2$:
+	ldr r2, [r0,#0x18]
+	tst r2, #0x40000000
+	bne wait2$
+
+	/* r1 = channel */
+	/* r2 = mail */
+	/* r3 = input channel */
+	ldr r2, [r0,#0]
+	and r3, r2, #0b1111
+	teq r3, r1
+	bne rightmail$
+
+	/* returns the top 28 bits of the mail */
+	and r0, r2, #0xfffffff0
+	pop {pc}
+
+
--- screen01/source/main.s
+++ screen01/source/main.s
@@ -7,33 +7,47 @@ _start:
 main:
 	mov sp, #0x8000
 
+	mov r0, #1024
+	mov r1, #768
+	mov r2, #16
+	bl InitializeFrameBuffer
+
+	teq r0, #0
+	bne noError$
+
 	mov r0, #16
 	mov r1, #1
 	bl SetGpioFunction
-
-	ldr r4, =pattern /* load address */
-	ldr r4, [r4]     /* load content */
-	mov r5, #0       /* sequence number */
-
-loop:	
-	/* r1 current pattern value */
-	mov r1, #1
-	lsl r1, r5
-	and r1, r4
-
 	mov r0, #16
+	mov r1, #0
 	bl SetGpio
-	ldr r0, =50000
-	bl wait
 
-	/* next index and wrap at 32 */
-	add r5, #1
-	cmp r5, #32
-	moveq r5, #0
+error$:
+	b error$
+
+noError$:
+	mov r4, r0
 	
-	b loop
+render$:
+	fbAddr .req r3
+	ldr r3, [r4, #32]
 
-.section .data
-.align 2
-pattern:
-	.int 0b11111111101010100010001000101010
+	color .req r0
+	y .req r1
+	mov y, #768
+drawRow$:
+	x .req r2
+	mov x, #1024
+drawPixel$:
+	strh color, [fbAddr]
+	add fbAddr, #2
+	sub x, #1
+	teq x, #0
+	bne drawPixel$
+
+	sub y, #1
+	add color, #1
+	teq y, #0
+	bne drawRow$
+
+	b render$
